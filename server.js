61575b28927cbf097a61cd6c3b7c792f39b5047d
sakib-xrz <sakibxrz21@gmail.com>
2024-06-30 13:01:33 +0600

    Update server.js to include cronJob functionality

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const app = require("./src/app.js");
 const { config } = require("./src/config/config.js");
 const connectDB = require("./src/config/db.js");
+const cornJob = require("./src/utils/cronJob.js");
 
 const startServer = async () => {
   // Connect database
   await connectDB();
-
+  cornJob();
   const port = config.port || 8000;
 
   app.listen(port, () => {
