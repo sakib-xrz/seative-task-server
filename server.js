e4cee51755a06deb21b6db7115648b4b8884276c
sakib-xrz <sakibxrz21@gmail.com>
2024-06-30 13:23:41 +0600

    Remove cronJob functionality from server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const app = require("./src/app.js");
 const { config } = require("./src/config/config.js");
 const connectDB = require("./src/config/db.js");
-const cornJob = require("./src/utils/cronJob.js");
 
 const startServer = async () => {
   // Connect database
   await connectDB();
-  cornJob();
   const port = config.port || 8000;
 
   app.listen(port, () => {
